4c701c2a8b20be6b9308013fbe3da4895854f401
chaarke <clark.ce12@gmail.com>
2023-01-14 16:53:12 -0500

    tweaks

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,6 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import React, { useState } from "react";
-import {Card, Button, IconButton, Provider as PaperProvider} from 'react-native-paper';
+import { Card, Button, IconButton, Provider as PaperProvider } from 'react-native-paper';
 
 type LotProps = {
   spaces: number;
@@ -12,17 +12,17 @@ type LotList = {
   lots: LotProps[]
 }
 
-function Lot({spaces, isFavorite, name}: LotProps): JSX.Element {
+function Lot({ spaces, isFavorite, name }: LotProps): JSX.Element {
   const [favorite, setFavorite] = useState(isFavorite);
-  
+
   return (
-    <Card style={{marginBottom: 10}}>
+    <Card style={{ marginBottom: 10 }}>
       <Card.Title
         title={`${spaces} Spots Remaining`}
         subtitle={`at ${name} lot`}
       />
       <Card.Actions>
-          {favorite ? <IconButton icon='heart' mode={"contained"} onPress={() => setFavorite(false)}/> : <Button icon='heart-outline' mode={"contained"} onPress={() => setFavorite(true)}>Favorite</Button>}
+        {favorite ? <IconButton icon='heart' mode={"contained"} onPress={() => setFavorite(false)} /> : <Button icon='heart-outline' mode={"contained"} onPress={() => setFavorite(true)}>Favorite</Button>}
       </Card.Actions>
     </Card>
   );
@@ -30,29 +30,33 @@ function Lot({spaces, isFavorite, name}: LotProps): JSX.Element {
 
 function LotList(list: LotList): JSX.Element {
 
-  let favorites = list.lots.filter( (lot) => lot.isFavorite );
-  let nonfavorites = list.lots.filter( (lot) =>  !lot.isFavorite );
+  let favorites = list.lots.filter((lot) => lot.isFavorite);
+  //favorites.sort((a, b) => (a.spaces > b.spaces ? 1 : 0));
+  let nonfavorites = list.lots.filter((lot) => !lot.isFavorite);
+  //favorites.sort((a,b) => (a.spaces > b.spaces ? 1 : 0));
 
   let sorted = favorites.concat(nonfavorites);
-  const lotlist = sorted.map((lot, idx) => <Lot spaces={lot.spaces} isFavorite={lot.isFavorite} name={lot.name} key={idx}/> )
+  const lotlist = sorted.map((lot, idx) => <Lot spaces={lot.spaces} isFavorite={lot.isFavorite} name={lot.name} key={idx} />)
 
   return (
-    <View style={{margin: 5}}>{lotlist}</View>
+    <View style={{ margin: 5 }}>{lotlist}</View>
   )
 }
 
 function HomePage(): JSX.Element {
-  let lot: LotProps = {spaces: 10, isFavorite: true, name: 'West'};
-  let lot2: LotProps = {spaces: 3, isFavorite: false, name: 'Library'}
-  let lot3: LotProps = {spaces: 5, isFavorite: true, name: 'Hackfeld'}
+  let lot: LotProps = { spaces: 5, isFavorite: true, name: 'West' };
+  let lot2: LotProps = { spaces: 3, isFavorite: false, name: 'Library' }
+  let lot3: LotProps = { spaces: 10, isFavorite: true, name: 'Hackfeld' }
 
   return (
-    <PaperProvider>
-      <LotList lots={[lot, lot2, lot3]} />
-      <Button icon={'refresh'}>
+    <SafeAreaView>
+      <PaperProvider>
+        <LotList lots={[lot, lot2, lot3]} />
+        <Button icon={'refresh'}>
           Reload Data
-      </Button>
-    </PaperProvider>
+        </Button>
+      </PaperProvider>
+    </SafeAreaView>
   )
 }
 
